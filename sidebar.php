e4b868b0d7ddb06cffbaad56519566b00839f562
bangkoor <akwijayanto@apps.ipb.ac.id>
2019-02-23 13:52:19 +0700

    Atur paginasi

--- sidebar.php
+++ sidebar.php
@@ -20,7 +20,7 @@
             </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="index.html">
+            <a class="nav-link" href="index.php">
               <i class="menu-icon mdi mdi-television"></i>
               <span class="menu-title">Dasbor</span>
             </a>

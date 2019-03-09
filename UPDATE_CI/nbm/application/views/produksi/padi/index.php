b76e03cb66703dc3f4fa0083d8747772dfa9b346
hudji <shudjimartsu@gmail.com>
2019-03-09 22:50:32 +0700

    update CI
    databasenya baru user aja mas

---
+++ UPDATE_CI/nbm/application/views/produksi/padi/index.php
@@ -0,0 +1,197 @@
+<div class="col-12 grid-margin">
+              <div class="card">
+                <div class="card-body">
+                  <h3><?php echo ucfirst($this->uri->segment(1)); ?></h3>
+                  <h4>
+                      Jenis Bahan Makanan <?php echo ucfirst($this->uri->segment(2)); ?>
+                  </h4>
+                  <div class="center">
+                  <div class="btn-group btn-block center mt-2" role="group" aria-label="Basic example">
+                          <button type="button" class="btn btn-outline-secondary btn-block">Q 1</button>
+                          <button type="button" class="btn btn-secondary disabled">></button>
+                          <button type="button" class="btn btn-secondary btn-block disabled">Q 2</button>
+                          <button type="button" class="btn btn-secondary disabled">></button>
+                          <button type="button" class="btn btn-secondary btn-block disabled">Q 3</button>
+                   </div>
+               	   </div>
+                  <?php echo form_open(base_url().'produksi/padi','class="form-horizontal form-data"', array("id" => "form-data"));?>
+                    <p class="card-title mt-4">
+                      Gabah / <i>Unhusked Rice</i>
+                    </p>
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Kotor</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Konversi</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Bersih</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <p class="card-title mt-4">
+                      Beras / <i>Rice</i>
+                    </p>
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Kotor</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Konversi</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Bersih</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <p class="card-title mt-4">
+                      Jagung / <i>Maize</i>
+                    </p>
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Kotor</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Konversi</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Bersih</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <p class="card-title mt-4">
+                      Jagung Basah / <i>Fresh Maize</i>
+                    </p>
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Kotor</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Konversi</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Bersih</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <p class="card-title mt-4">
+                      Gandum / <i>Wheat</i>
+                    </p>
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Kotor</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Konversi</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Bersih</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <p class="card-title mt-4">
+                      Tepung Gandum / <i>Wheat Flour</i>
+                    </p>
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Kotor</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Konversi</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Produksi Bersih</label>
+                          <div class="col-sm-9">
+                            <input type="text" class="form-control" />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <button type="submit" name="submit" class="btn btn-success btn-fw">Submit</button>
+                    <button type="next" name="next" class="btn btn-primary btn-fw float-right">Next</button>
+                  </form>
+                </div>
+              </div>
+            </div>
